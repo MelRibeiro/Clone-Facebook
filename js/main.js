e468afd7978a0124913343f8a0f66154a6395a85
MelRibeiro <melissacristine2015@gmail.com>
2021-12-11 16:07:00 -0300

    parte de nascimento feita

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
+let year = [];
 
+function createOptionYear(total) {
+  for (let i = 2021; i > 1920; i--) {
+    total = year.push(i);
+    console.log(year);
+  }
+}
+
+createOptionYear();
+
+const select = document.getElementById("select");
+
+year.forEach(function (element, value) {
+  select.appendChild(new Option(element, value));
+});
+
+let day = [];
+
+function createOptionDay(total) {
+  for (let i = 1; i <= 31; i++) {
+    total = day.push(i);
+    console.log(day);
+  }
+}
+
+createOptionDay();
+
+const idDay = document.getElementById("idDay");
+
+day.forEach(function (element, value) {
+  idDay.appendChild(new Option(element, value));
+});
+
+let month = [
+  "Jan",
+  "Fev",
+  "Mar",
+  "Abr",
+  "Mai",
+  "Jun",
+  "Jul",
+  "Ago",
+  "Set",
+  "Out",
+  "Nov"
+];
+
+const idMonth = document.getElementById("idMonth");
+
+month.forEach(function (element, value) {
+  idMonth.appendChild(new Option(element, value));
+});
